a8c851209e8ccfccff81968aeeb7a868a2fe0d3b
Igor Alfeza <igoralfeza@gmail.com>
2022-11-23 16:28:56 -0300

    Exercício-do-dia-07

--- fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/scripts.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/scripts.js
@@ -94,3 +94,26 @@ switch (chessPiece.toLowerCase()) {
     console.log('Erro, peça inválida!');
     break;
 };
+
+//Exercício 07//
+
+const nota = 75;
+
+if (nota < 0 || nota > 100 ) {
+    console.log("Erro");
+} else if ( nota >= 90) {
+    console.log("A");
+} else if (nota < 90 && nota >= 80) {
+    console.log("B");
+} else if (nota < 80 && nota >= 70) {
+    console.log("C");
+} else if (nota < 70 && nota >= 60) {
+    console.log("D");
+} else if (nota < 60 && nota >= 50) {
+    console.log("E");
+} else if (nota < 50) {
+    console.log("F");
+}
+
+//Exercício 08//
+
