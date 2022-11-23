c9783b9aa4f4d63f61677287cf466c447b197c32
Igor Alfeza <igoralfeza@gmail.com>
2022-11-23 15:49:57 -0300

    Exerc ício-do-dia-03

--- fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/scripts.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/scripts.js
@@ -11,11 +11,25 @@ console.log (a % b);
 
 //Exercício 02//
 
-const joaoNote = 5
-const mariaNote = 7
+const joaoNote = 5;
+const mariaNote = 7;
 
 if (joaoNote > mariaNote) {
     console.log(joaoNote);
 } else { 
     console.log(mariaNote);
+}
+
+//Exercício 03//
+
+const marcelaNote = 5;
+const marianaNote = 7;
+const joseNote = 9;
+
+if (marcelaNote > mariaNote && marcelaNote > joseNote) {
+    console.log(marcelaNote);
+} else if (marianaNote > joseNote && marianaNote > marcelaNote) { 
+    console.log(marianaNote);
+} else if (joseNote > marcelaNote && joseNote > marianaNote) { 
+    console.log(joseNote);
 }
