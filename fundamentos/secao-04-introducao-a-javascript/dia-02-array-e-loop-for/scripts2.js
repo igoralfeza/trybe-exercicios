f2d3840da7faa59c910b7ff98594546455449183
Igor Alfeza <igoralfeza@gmail.com>
2022-11-25 10:28:32 -0300

    exercicios-de-fixação-3

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/scripts2.js
@@ -0,0 +1,25 @@
+/* let numeros = [1,2,3,4,5];
+
+
+for(let numero of numeros) {
+  console.log(numero);
+} */
+
+
+/* let word = 'Hello';
+for (let letter of word) {
+  console.log(letter);
+} */
+
+/* let arrOfNumbers = [10, 20, 30];
+for (let sum of arrOfNumbers) {
+  sum += 10;
+  console.log(sum);
+} */
+
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let name of names) {
+    console.log(name)
+}
