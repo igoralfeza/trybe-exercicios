38b1cb2d83d0c2913ab3af74bb224f5df9a85620
Igor Alfeza <igoralfeza@gmail.com>
2022-11-25 10:22:16 -0300

    exercicios-de-fixaçao-2

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/scripts1.js
@@ -0,0 +1,45 @@
+
+/* 
+let numero = 10
+
+
+for (let contador = 1; contador <= 9; contador ++) {
+    console.log("");
+    console.log(numero * contador)
+} */
+
+
+
+/* let lisdaDeNomes = ["Joana","Maria", "Lucas"];
+
+
+for (let indice = 0; indice < lisdaDeNomes.length; indice++) {
+    let mensagem = "Boas Vindas, " + lisdaDeNomes[indice] +
+     "!";
+     console.log(mensagem)
+
+} */
+
+/* let cars = ['Saab', 'Volvo', 'BMW'];
+
+
+for (let index = 0; index < cars.length; index ++) {
+  console.log(cars[index]);
+} */
+
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+
+for (let index = 0;index < groceryList.length; index ++) {
+    console.log(groceryList[index])
+}
+
+
+
+
+
+
+
+
+
